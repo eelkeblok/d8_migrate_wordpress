8936494ed096aa0b01d05685b4177d58d1073698
Eelke Blok <eelke.blok@oneshoe.nl>
2016-03-09 20:35:21 +0100

    Create processplugin to allow a date to use a fallback value

--- src/Plugin/migrate/process/WordPressDate.php
+++ src/Plugin/migrate/process/WordPressDate.php
@@ -12,22 +12,30 @@ use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Row;
 
 /**
- * This plugin turns a WordPress data (datetime database column) into a UNIX
+ * This plugin turns a WordPress date (datetime database column) into a UNIX
  * timestamp. Provide a source value to act as a default using the default
  * property.
  *
  * @MigrateProcessPlugin(
- *   id = "wordpress_data"
+ *   id = "wordpress_date"
  * )
  */
 
-class WordPressDate extends ProcessPluginBase { {
+class WordPressDate extends ProcessPluginBase {
   /**
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $return = strtotime($value);
+    if (empty($value) || $value == '0000-00-00 00:00:00') {
+      $source = $row->getSource();
+      if (!empty($this->configuration['fallback']) && !empty($source[$this->configuration['fallback']])) {
+        $value = $source[$this->configuration['fallback']];
+      }
+      else {
+        $value = '1970-01-01 12:00:00';
+      }
+    }
 
-    return $return;
+    return strtotime($value);
   }
 }
